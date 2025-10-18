38b2fa2802b9b6b093a55dfa85f22acd53c4ce43
ManuelDLC10 <manueldlc.z10@gmail.com>
2025-10-18 12:29:46 -0400

    hw2 part 1

--- Assets/Hands-On Homework #2/Scripts/HW2PlayerShoot.cs
+++ Assets/Hands-On Homework #2/Scripts/HW2PlayerShoot.cs
@@ -2,5 +2,16 @@ using UnityEngine;
 
 public class HW2PlayerShoot : MonoBehaviour
 {
+    public GameObject prefab;
+    public Transform bulletTrash;
+    public Transform bulletSpawn;
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            GameObject bullet = Instantiate(prefab, bulletSpawn.position, Quaternion.identity);
+            bullet.transform.SetParent(bulletTrash);
+        }
+    }
 }
