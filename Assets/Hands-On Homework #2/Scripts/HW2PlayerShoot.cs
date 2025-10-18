6f2129a81fc80f447bfa329aaa91618cc032d2a7
ManuelDLC10 <manueldlc.z10@gmail.com>
2025-10-18 15:58:33 -0400

    HW2 Final

--- Assets/Hands-On Homework #2/Scripts/HW2PlayerShoot.cs
+++ Assets/Hands-On Homework #2/Scripts/HW2PlayerShoot.cs
@@ -5,13 +5,51 @@ public class HW2PlayerShoot : MonoBehaviour
     public GameObject prefab;
     public Transform bulletTrash;
     public Transform bulletSpawn;
+    public GameObject SBullet;
+
+    private const float Timer = 0.5f;
+    private float _currentTime = 0.5f;
+    private bool _canShoot = true;
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        TimerMethod();
+        Shoot();
+
+    }
+
+    private void TimerMethod()
+    {
+        if (!_canShoot)
+        {
+            _currentTime -= Time.deltaTime;
+
+            if (_currentTime < 0)
+            {
+                _canShoot = true;
+                _currentTime = Timer;
+            }
+        }
+    }
+
+    private void Shoot()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0) && _canShoot)
         {
             GameObject bullet = Instantiate(prefab, bulletSpawn.position, Quaternion.identity);
-            //bullet.transform.SetParent(bulletTrash);
+            bullet.transform.SetParent(bulletTrash);
+
+            _canShoot = false;
         }
+
+            if (Input.GetKeyDown(KeyCode.Mouse1) && _canShoot)
+            {
+                GameObject bullet = Instantiate(SBullet, bulletSpawn.position, Quaternion.identity);
+                bullet.transform.SetParent(bulletTrash);
+
+                _canShoot = false;
+            }
+        
+
     }
 }
